8f99a290e090f038fa8e3a902f4d764a062a4033
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-02-28 18:18:12 +0000

    added styles and navbar

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<nav class="container-fluid">
+	<ul>
+		<li>
+			<a class="contrast" href="/"><strong>Page</strong></a>
+		</li>
+	</ul>
+	<ul>
+		<li><a href="/">Link 1</a></li>
+		<li><a href="/">Link 2</a></li>
+	</ul>
+</nav>
+
+<main>
+	<section class="container">
+		<slot />
+	</section>
+</main>
+
+<style global>
+	@import '@picocss/pico';
+</style>
