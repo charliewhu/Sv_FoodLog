a6ba6d103a4e2786250eb25fd2be19827b769bbf
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-03-01 15:23:15 +0000

    corrected test name

--- tests/delete-item.spec.ts
+++ tests/delete-item.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from './base.ts';
 import { prisma } from '../src/lib/server/prisma.ts';
 
-test('create food item', async ({ page }) => {
+test('delete food item', async ({ page }) => {
 	// Given there is an item in the database
 	await prisma.food.create({
 		data: {
