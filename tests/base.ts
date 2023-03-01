66da1e6f0660ca975d681b2c1721d4e505ace154
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-03-01 05:52:07 +0000

    added fixture

---
+++ tests/base.ts
@@ -0,0 +1,18 @@
+import { test as base } from '@playwright/test';
+
+export const test = base.extend({
+	page: async ({ page }, use) => {
+		// We have a few cases where we need our app to know it's running in Playwright.
+		// This is inspired by Cypress that auto-injects window.Cypress.
+		// await page.addInitScript(() => {
+		// 	(window as any).Playwright = true;
+		// });
+
+		console.log('Running');
+		await page.goto('/');
+
+		await use(page);
+	}
+});
+
+export { expect } from '@playwright/test';
--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,9 @@
-import { expect, test } from '@playwright/test';
+//import { expect, test } from '@playwright/test';
+//import { expect } from '@playwright/test';
+import { test, expect } from './base.ts';
 
 test('index page has expected h1', async ({ page }) => {
-	await page.goto('/');
+	//await page.goto('/')
+	console.log(process.env.DATABASE_URL);
 	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
 });
