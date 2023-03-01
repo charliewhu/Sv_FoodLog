b57adbc85bcbbfdf247a488f867391829b51a3da
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-03-01 10:00:50 +0000

    forced playwright to test against dev server

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,8 +2,8 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
 	webServer: {
-		command: 'npm run build && npm run preview',
-		port: 4173
+		command: 'pnpm dev --port 7357',
+		port: 7357
 	},
 	testDir: 'tests'
 };
