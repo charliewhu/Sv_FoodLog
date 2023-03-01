a8e22dc55c50b651a860728efb5055991f8bc926
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-03-01 14:10:02 +0000

    adjusted timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,13 +10,7 @@ const config: PlaywrightTestConfig = {
 	/* Opt out of parallel tests*/
 	workers: 1,
 
-	expect: {
-		/**
-		 * Maximum time expect() should wait for the condition to be met.
-		 * For example in `await expect(locator).toHaveText();`
-		 */
-		timeout: 3000
-	}
+	timeout: 3000
 };
 
 export default config;
