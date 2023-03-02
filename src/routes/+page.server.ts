9991abb09b346d63990b97a1d637105c22807da9
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-03-02 06:51:23 -0500

    added try catch to food creation

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,21 +1,25 @@
 import { prisma } from '$lib/server/prisma';
-import { redirect, type Actions } from '@sveltejs/kit';
+import { redirect, type Actions, fail } from '@sveltejs/kit';
 
 export const actions: Actions = {
 	createFood: async ({ request }) => {
 		const form = await request.formData();
 		const food = Object.fromEntries(form);
 
-		await prisma.food.create({
-			data: {
-				name: String(food.name),
-				calories: Number(food.calories),
-				protein: Number(food.protein),
-				carb: Number(food.carb),
-				fat: Number(food.fat),
-				date: new Date()
-			}
-		});
+		try {
+			await prisma.food.create({
+				data: {
+					name: String(food.name),
+					calories: Number(food.calories),
+					protein: Number(food.protein),
+					carb: Number(food.carb),
+					fat: Number(food.fat),
+					date: new Date()
+				}
+			});
+		} catch (err) {
+			return fail(500, { message: 'could not create food' });
+		}
 
 		throw redirect(302, '/');
 	},
