d27d2715cd409f3ae82d1ba4e96fcfa83b36e8bd
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-03-02 06:23:50 -0500

    prevent form from expanding with list

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,17 +6,25 @@
 	$: foods = data.foods;
 </script>
 
-<article>
-	<form method="POST" action="?/createFood" use:enhance>
-		<div class="grid">
-			<input type="text" name="name" placeholder="Name" />
-			<input type="number" name="calories" placeholder="Calories" />
-		</div>
-		<div class="grid">
-			<input type="number" name="protein" placeholder="Protein" />
-			<input type="number" name="carb" placeholder="Carb" />
-			<input type="number" name="fat" placeholder="Fat" />
-		</div>
-		<button type="submit" aria-label="addFoodItem"> Add </button>
-	</form>
+<article class="foodForm">
+	<div>
+		<form method="POST" action="?/createFood" use:enhance>
+			<div class="grid">
+				<input type="text" name="name" placeholder="Name" />
+				<input type="number" name="calories" placeholder="Calories" />
+			</div>
+			<div class="grid">
+				<input type="number" name="protein" placeholder="Protein" />
+				<input type="number" name="carb" placeholder="Carb" />
+				<input type="number" name="fat" placeholder="Fat" />
+			</div>
+			<button type="submit" aria-label="addFoodItem"> Add </button>
+		</form>
+	</div>
 </article>
+
+<style>
+	.foodForm {
+		height: fit-content;
+	}
+</style>
