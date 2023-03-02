b3d17368aae1be1468888d8517915f3881a9c1d8
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-03-02 17:17:09 -0500

    add update item action

--- src/routes/[id]/update/+page.server.ts
+++ src/routes/[id]/update/+page.server.ts
@@ -1,5 +1,5 @@
 import { prisma } from '$lib/server/prisma.ts';
-import { error } from '@sveltejs/kit';
+import { error, type Actions, fail, redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ params }) => {
@@ -14,4 +14,31 @@ export const load: PageServerLoad = async ({ params }) => {
 	}
 
 	return { food: food };
+};
+
+export const actions: Actions = {
+	default: async ({ request, params }) => {
+		const form = await request.formData();
+		const food = Object.fromEntries(form);
+
+		try {
+			await prisma.food.update({
+				where: {
+					id: Number(params.id)
+				},
+				data: {
+					name: String(food.name),
+					calories: Number(food.calories),
+					protein: Number(food.protein),
+					carb: Number(food.carb),
+					fat: Number(food.fat)
+				}
+			});
+		} catch (err) {
+			console.log(err);
+			return fail(500, { message: 'Could not update' });
+		}
+
+		throw redirect(302, '/');
+	}
 };
