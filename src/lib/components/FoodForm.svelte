4cd972dffaef491ee2c8321f0e564635ff234f7c
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-03-02 07:59:40 -0500

    created FoodForm as component

---
+++ src/lib/components/FoodForm.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+</script>
+
+<article class="foodForm">
+	<div>
+		<form method="POST" action="?/createFood">
+			<div class="grid">
+				<input type="text" name="name" placeholder="Name" />
+				<input type="number" name="calories" placeholder="Calories" />
+			</div>
+			<div class="grid">
+				<input type="number" name="protein" placeholder="Protein" />
+				<input type="number" name="carb" placeholder="Carb" />
+				<input type="number" name="fat" placeholder="Fat" />
+			</div>
+			<button type="submit" aria-label="addFoodItem"> Add </button>
+		</form>
+	</div>
+</article>
+
+<style>
+	.foodForm {
+		height: fit-content;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,5 @@
 <script lang="ts">
-	import { enhance } from '$app/forms';
+	import FoodForm from '$lib/components/FoodForm.svelte';
 </script>
 
-<article class="foodForm">
-	<div>
-		<form method="POST" action="?/createFood" use:enhance>
-			<div class="grid">
-				<input type="text" name="name" placeholder="Name" />
-				<input type="number" name="calories" placeholder="Calories" />
-			</div>
-			<div class="grid">
-				<input type="number" name="protein" placeholder="Protein" />
-				<input type="number" name="carb" placeholder="Carb" />
-				<input type="number" name="fat" placeholder="Fat" />
-			</div>
-			<button type="submit" aria-label="addFoodItem"> Add </button>
-		</form>
-	</div>
-</article>
-
-<style>
-	.foodForm {
-		height: fit-content;
-	}
-</style>
+<FoodForm />
