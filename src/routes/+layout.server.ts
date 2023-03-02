4bd0726acf1823ca70dd451f9d10031fb8617846
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-03-02 06:51:43 -0500

    added date filter

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -4,7 +4,9 @@ import { prisma } from '$lib/server/prisma';
 export const load: PageServerLoad = async () => {
 	const startDate = new Date();
 	startDate.setUTCHours(0, 0, 0, 0);
-	const endDate = new Date(startDate.getTime());
+
+	const endDate = new Date();
+	endDate.setUTCHours(23, 59, 59, 999);
 
 	const foods = await prisma.food.findMany({
 		where: {
