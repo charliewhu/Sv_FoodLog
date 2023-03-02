dc6e4d15c277b99573123268c5de9217dd717a74
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-03-02 16:54:02 -0500

    created form component

--- src/lib/components/FoodForm.svelte
+++ src/lib/components/FoodForm.svelte
@@ -1,19 +1,26 @@
 <script lang="ts">
+	import type { Food } from '@prisma/client';
+
+	export let food: Food;
 </script>
 
-<article class="foodForm">
+<article data-testId="foodForm" class="foodForm">
 	<div>
 		<form method="POST" action="?/createFood">
 			<div class="grid">
-				<input type="text" name="name" placeholder="Name" />
-				<input type="number" name="calories" placeholder="Calories" />
+				<input type="text" name="name" placeholder="Name" value={food?.name} />
+				<input type="number" name="calories" placeholder="Calories" value={food?.calories} />
 			</div>
 			<div class="grid">
-				<input type="number" name="protein" placeholder="Protein" />
-				<input type="number" name="carb" placeholder="Carb" />
-				<input type="number" name="fat" placeholder="Fat" />
+				<input type="number" name="protein" placeholder="Protein" value={food?.protein} />
+				<input type="number" name="carb" placeholder="Carb" value={food?.carb} />
+				<input type="number" name="fat" placeholder="Fat" value={food?.fat} />
 			</div>
-			<button type="submit" aria-label="addFoodItem"> Add </button>
+			{#if !food}
+				<button type="submit" aria-label="addFoodItem"> Add </button>
+			{:else}
+				<button type="submit" aria-label="submitUpdateFoodItem"> Update </button>
+			{/if}
 		</form>
 	</div>
 </article>
