7df74ee00c0566d749be175d72a6d5ae713c9ae0
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-03-02 17:01:50 -0500

    styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -31,17 +31,11 @@
 								<div>{food.protein}</div>
 								<div>{food.carb}</div>
 								<div>{food.fat}</div>
-								<div>
-									<a href={`/${food.id}/update`}>
-										<button type="button" aria-label="updateFoodItem"> Edit </button>
-									</a>
-								</div>
-								<div>
-									<form method="POST" action="?/deleteFood">
-										<input type="hidden" name="id" value={food.id} />
-										<button type="submit" aria-label="deleteFoodItem"> X </button>
-									</form>
-								</div>
+								<a href={`/${food.id}/update`} role="button" aria-label="updateFoodItem"> Edit </a>
+								<form method="POST" action="?/deleteFood">
+									<input type="hidden" name="id" value={food.id} />
+									<button type="submit" aria-label="deleteFoodItem"> X </button>
+								</form>
 							</div>
 						</div>
 					{/each}
