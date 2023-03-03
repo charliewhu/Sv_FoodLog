c52051847f5f3b746290eb59d98e0eec8878db27
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-03-03 06:16:34 -0500

    added asertions

--- tests/list-items.spec.ts
+++ tests/list-items.spec.ts
@@ -5,6 +5,7 @@ test('list items', async ({ page }) => {
 	const today = new Date();
 	const yesterday = new Date();
 	yesterday.setDate(yesterday.getDate() - 1);
+	const yDate = new Date(yesterday);
 
 	const startDate = new Date();
 	startDate.setUTCHours(0, 0, 0, 0);
@@ -46,7 +47,7 @@ test('list items', async ({ page }) => {
 
 	await prisma.food.create({
 		data: {
-			date: new Date(yesterday),
+			date: yDate,
 			name: 'Bread',
 			protein: 4,
 			carb: 15,
@@ -75,4 +76,18 @@ test('list items', async ({ page }) => {
 	const firstFood = await prisma.food.findFirst();
 	const firstFoodName = firstFood?.name;
 	await expect(page.getByText(firstFoodName)).toBeVisible();
+
+	// When I navigate back to yesterday
+	await page.getByTestId('prevDay').click();
+
+	// Then I will see yesterdays food item in the list
+	const lastFood = await prisma.food.findMany({
+		where: {
+			date: yDate
+		}
+	});
+	await expect(page.getByText(lastFood[0].name)).toBeVisible();
+
+	// And not todays food
+	await expect(page.getByText(firstFoodName)).not.toBeVisible();
 });
