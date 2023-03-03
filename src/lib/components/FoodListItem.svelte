565c09f693e9a865d8acb180a79f100cc08ca4ba
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-03-03 07:50:04 -0500

    created FoodListItem component

---
+++ src/lib/components/FoodListItem.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import type { Food } from '@prisma/client';
+
+	export let food: Food;
+</script>
+
+<div data-testId="foodListItem" class="grid list-item">
+	<div>{food.name}</div>
+	<div>{food.calories}</div>
+	<div>{food.protein}</div>
+	<div>{food.carb}</div>
+	<div>{food.fat}</div>
+	<a href={`/${food.id}/update`} role="button" aria-label="updateFoodItem"> Edit </a>
+	<form method="POST" action="?/deleteFood">
+		<input type="hidden" name="id" value={food.id} />
+		<button type="submit" aria-label="deleteFoodItem"> X </button>
+	</form>
+</div>
+
+<style>
+	/* your styles go here */
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import FoodListItem from '$lib/components/FoodListItem.svelte';
 	import type { LayoutData } from './$types';
 
 	export let data: LayoutData;
@@ -23,22 +24,11 @@
 			<article>
 				{#if foods.length !== 0}
 					<h5>Your Foods</h5>
-					{#each foods as food}
-						<div data-testId="foodList">
-							<div data-testId="foodListItem" class="grid list-item">
-								<div>{food.name}</div>
-								<div>{food.calories}</div>
-								<div>{food.protein}</div>
-								<div>{food.carb}</div>
-								<div>{food.fat}</div>
-								<a href={`/${food.id}/update`} role="button" aria-label="updateFoodItem"> Edit </a>
-								<form method="POST" action="?/deleteFood">
-									<input type="hidden" name="id" value={food.id} />
-									<button type="submit" aria-label="deleteFoodItem"> X </button>
-								</form>
-							</div>
-						</div>
-					{/each}
+					<div data-testId="foodList">
+						{#each foods as food}
+							<FoodListItem {food} />
+						{/each}
+					</div>
 				{:else}
 					<p>You haven't logged any food today</p>
 				{/if}
